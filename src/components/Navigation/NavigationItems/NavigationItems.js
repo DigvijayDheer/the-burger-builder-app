053236169d17a06292d8b55b05d9b62bb168aac9
DigvijayDheer <digvijaydheersaini@gmail.com>
2020-12-20 22:12:24 +0530

    upload-main

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = ( props ) => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" exact>BURGER BUILDER</NavigationItem>
+        {props.isAuthenticated ? <NavigationItem link="/orders">ORDERS</NavigationItem> : null }
+        {!props.isAuthenticated
+            ? <NavigationItem link="/auth">LOGIN</NavigationItem>
+            : <NavigationItem link="/logout">LOGOUT</NavigationItem>}
+    </ul>
+);
+
+export default navigationItems;
